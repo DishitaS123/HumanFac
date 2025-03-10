c7686150129160dbe0c7f10fbd129a78a30a2f3c
Nick Landry <nglandry@mines.edu>
2025-03-10 16:50:13 -0600

    adding code to remove english characters from the text

--- translate.py
+++ translate.py
@@ -19,9 +19,15 @@ def clean_text(text):
 
 # Asynchronous function to translate text to English
 async def translate_to_english(text):
-    translator = Translator()
-    translated = await translator.translate(text, dest='en')
-    return translated.text
+    try:
+        translator = Translator()
+        translated = await translator.translate(text, dest='en')
+        return translated.text
+    except Exception as e:
+        print(f"Error translating text: {e}")
+        print(f"Sleeping for {60} seconds")
+        time.sleep(60)
+        return await translate_to_english(text)
 
 # # Asynchronous function to translate a list of texts
 # async def translate_list(text_list):
@@ -36,13 +42,13 @@ def split_text(text):
     inital_parts = re.split(r"(?=\{'from': 'human',|\{'from': 'gpt',)", text)
     # Filter out empty strings
     parts = []
-    print(f"{len(inital_parts)} parts in intial split")
+    # print(f"{len(inital_parts)} parts in intial split")
     for part in re.split(r"(?=\{'from': 'human',|\{'from': 'gpt',)", text):
         if part:
             chunks = []
             if(len(part) > 5000):
                 chunks = [part[i:i+5000] for i in range(0, len(part), 5000)]
-                print(f"splitting into {len(chunks)} chunks")
+                # print(f"splitting into {len(chunks)} chunks")
             else:
                 chunks = [part]
 
@@ -60,7 +66,7 @@ def split_text(text):
 def process_and_translate_row(row):
     first_col = row[0]
     translated_texts = []
-    print(f"Splitting row into {len(row[1:])} parts")
+    # print(f"Splitting row into {len(row[1:])} parts")
     for col in row[1:]:
         split_texts = split_text(str(col))
         translated_texts.append(' '.join(split_texts))
@@ -84,13 +90,33 @@ def split_and_save_csv(input_csv_path, output_csv_path):
 
     print("Processing complete. Output saved to:", output_csv_path)
 
-# input_csv_path = 'Datasets/Dataframe_Wrangled_NoDuplicates.csv'
-# output_csv_path = 'Datasets/NoDuplicates_Translated.csv'
-# split_and_save_csv(input_csv_path, output_csv_path)
+def eliminate_english(input_csv_path, elim_output_path):
+    df = pd.read_csv(input_csv_path)
 
-for file in os.listdir('Datasets/Split_CSV'):
-    input_csv_path = f'Datasets/Split_CSV/{file}'
-    output_csv_path = f'Datasets/Split_CSV_Translated/{file}'
-    split_and_save_csv(input_csv_path, output_csv_path)
-    time.sleep(60)
-    print(f"Sleeping for {60} seconds")
+    # parse conversations
+    text_list = []
+    text_arrays = []
+    for conversation in df["conversations"]:
+        text = ""
+        text_arr = []
+        parsed_conversation = eval(conversation)
+        for dialogue in parsed_conversation:
+            text += dialogue["value"] + " "
+            text_arr.append(dialogue["value"])
+        text_list.append(text)
+        text_arrays.append(text_arr)
+
+    # create column with the conversation text
+    df["text"] = text_list
+    df["values"] = text_arrays
+
+input_csv_path = 'Datasets/Dataframe_Wrangled_NoDuplicates.csv'
+output_csv_path = 'Datasets/NoDuplicates_Translated.csv'
+split_and_save_csv(input_csv_path, output_csv_path)
+
+# for file in os.listdir('Datasets/Split_CSV'):
+#     input_csv_path = f'Datasets/Split_CSV/{file}'
+#     output_csv_path = f'Datasets/Split_CSV_Translated/{file}'
+#     split_and_save_csv(input_csv_path, output_csv_path)
+#     time.sleep(60)
+#     print(f"Sleeping for {60} seconds")
