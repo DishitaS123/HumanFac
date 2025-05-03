c16845929f55f0dc9d3ff6d79c75238b9723ffb8
DishitaS123 <dsharma1@mines.edu>
2025-05-03 15:08:31 -0600

    fixed Data wrangle to avoide same conversation twice

--- Data_Wrangle.py
+++ Data_Wrangle.py
@@ -1,33 +1,25 @@
 import json 
 import pandas as pd
 
-def pull_from_JSON(filename):
+def from_JSON(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         data = json.load(f)
-    #list to store all filtered conversation "/code"
+    # list to store all filtered conversations with "/code"
     filtered_list = []
     for conversation in data:
         for text in conversation["conversations"]:
             if '/code' in text['value']:
                 filtered_list.append(conversation)
-                break 
-             
+                break
     return filtered_list
 
 #call function
-file_1 = pull_from_JSON('/Users/dishita/Desktop/HumanFac_Data/sg_90k_part1.json')
-file_2 = pull_from_JSON('/Users/dishita/Desktop/HumanFac_Data/sg_90k_part2.json')
+file_1 = from_JSON('YOUR FILE PATH/sg_90k_part1.json')
+file_2 = from_JSON('YOUR FILE PATH/sg_90k_part2.json')
 
 #concantenate lists 
 combined = file_1 + file_2
 df = pd.DataFrame(combined)
 df.to_csv('Dataframe_Combined_Wrangled.csv', index=False)
-
-#create and save dataframe
-#df1 = pd.DataFrame(file_1)
-#df1.to_csv('Dataframe_Combined_Wrangled_part1.csv', index=False)
-
-#df2 = pd.DataFrame(file_2)
-#df2.to_csv('Dataframe_Combined_Wrangled_part2.csv', index=False)
 
 print(f"Number of entries: {len(df)}")
